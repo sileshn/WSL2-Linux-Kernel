efd25a6aff8368b7bd3df3f6aac280650e12bce7
Nathan Chancellor <nathan@kernel.org>
2021-01-27 11:08:30 -0700

    wsl2_defconfig: Enable/disable a bunch of hardening options
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- bin/gen_wsl_config.sh
+++ bin/gen_wsl_config.sh
@@ -44,4 +44,43 @@ curl -LSso "${CONFIG}" https://github.com/microsoft/WSL2-Linux-Kernel/raw/linux-
     --set-str LOCALVERSION "-microsoft-cbl" \
     -u FRAME_WARN
 
+# Enable/disable a bunch of checks based on kconfig-hardened-check
+# https://github.com/a13xp0p0v/kconfig-hardened-check
+./scripts/config \
+    --file "${CONFIG}" \
+    -d AIO \
+    -d DEBUG_FS \
+    -d DEVMEM \
+    -d HARDENED_USERCOPY_FALLBACK \
+    -d INIT_STACK_NONE \
+    -d KSM \
+    -d LEGACY_PTYS \
+    -d PROC_KCORE \
+    -d VT \
+    -d X86_IOPL_IOPERM \
+    -e BUG_ON_DATA_CORRUPTION \
+    -e DEBUG_CREDENTIALS \
+    -e DEBUG_LIST \
+    -e DEBUG_NOTIFIERS \
+    -e DEBUG_SG \
+    -e DEBUG_VIRTUAL \
+    -e DEBUG_WX \
+    -e FORTIFY_SOURCE \
+    -e HARDENED_USERCOPY \
+    -e INIT_STACK_ALL \
+    -e INIT_STACK_ALL_ZERO \
+    -e INTEGRITY \
+    -e LOCK_DOWN_KERNEL_FORCE_CONFIDENTIALITY \
+    -e SECURITY_LOADPIN \
+    -e SECURITY_LOADPIN_ENFORCE \
+    -e SECURITY_LOCKDOWN_LSM \
+    -e SECURITY_LOCKDOWN_LSM_EARLY \
+    -e SECURITY_SAFESETID \
+    -e SECURITY_YAMA \
+    -e SLAB_FREELIST_HARDENED \
+    -e SLAB_FREELIST_RANDOM \
+    -e SLUB_DEBUG \
+    -e SHUFFLE_PAGE_ALLOCATOR \
+    --set-val ARCH_MMAP_RND_BITS 32
+
 ./bin/build.sh -u
