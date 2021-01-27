0c358ac89f822b74a4fca4378ac59d97fa0af3f1
Nathan Chancellor <nathan@kernel.org>
2021-01-27 11:07:55 -0700

    bin/build.sh: Default to -Werror
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- bin/build.sh
+++ bin/build.sh
@@ -86,6 +86,7 @@ function kmake() {
         HOSTCXX="${HOSTCXX}" \
         HOSTLD="${HOSTLD}" \
         HOSTLDFLAGS="${HOSTLDFLAGS}" \
+        KCFLAGS="${KCFLAGS--Werror}" \
         LD="${LD}" \
         LLVM="${LLVM}" \
         LLVM_IAS="${LLVM_IAS}" \
