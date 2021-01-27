e82a2afbbe966ac20aab25238c83560be8e4ed89
Nathan Chancellor <nathan@kernel.org>
2021-01-27 11:08:24 -0700

    wsl2_defconfig: Initial personal tuning
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- bin/gen_wsl_config.sh
+++ bin/gen_wsl_config.sh
@@ -24,4 +24,24 @@ curl -LSso "${CONFIG}" https://github.com/microsoft/WSL2-Linux-Kernel/raw/linux-
     -e VIRTIO_PCI \
     -e NET_9P_VIRTIO
 
+# Initial tuning
+#   * FTRACE: Limit attack surface and avoids a warning at boot.
+#   * MODULES: Limit attack surface and we don't support them anyways.
+#   * LTO_CLANG: Optimization.
+#   * CFI_CLANG: Hardening.
+#   * LOCALVERSION_AUTO: Helpful when running development builds.
+#   * LOCALVERSION: Replace 'standard' with 'cbl' since this is a Clang built kernel.
+#   * FRAME_WARN: The 64-bit default is 2048. Clang uses more stack space so this avoids build-time warnings.
+./scripts/config \
+    --file "${CONFIG}" \
+    -d FTRACE \
+    -d MODULES \
+    -d LTO_NONE \
+    -e LTO_CLANG \
+    -e LTO_CLANG_THIN \
+    -e CFI_CLANG \
+    -e LOCALVERSION_AUTO \
+    --set-str LOCALVERSION "-microsoft-cbl" \
+    -u FRAME_WARN
+
 ./bin/build.sh -u
