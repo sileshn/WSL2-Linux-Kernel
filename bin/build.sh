b2c9ac1457f9087c4a1603b5e26f24a1a742018c
Nathan Chancellor <nathan@kernel.org>
2021-02-17 14:42:51 -0700

    bin/build.sh: Initial baseline script
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

---
+++ bin/build.sh
@@ -0,0 +1,126 @@
+#!/usr/bin/env bash
+
+set -eu
+
+BASE=$(cd "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"/.. && pwd)
+
+# Get parameters
+function parse_parameters() {
+    while ((${#})); do
+        case ${1} in
+            *=*) export "${1?}" ;;
+            -i | --incremental) INCREMENTAL=true ;;
+            -j | --jobs) JOBS=${1} ;;
+            -u | --update-config-only) UPDATE_CONFIG_ONLY=true ;;
+            -v | --verbose) VERBOSE=true ;;
+        esac
+        shift
+    done
+
+    # Handle architecture specific variables
+    case ${ARCH:=x86_64} in
+        x86_64)
+            CONFIG=arch/x86/configs/wsl2_defconfig
+            KERNEL_IMAGE=bzImage
+            ;;
+
+        # We only support x86 at this point but that might change eventually
+        *)
+            echo "\${ARCH} value of '${ARCH}' is not supported!" 2>&1
+            exit 22
+            ;;
+    esac
+}
+
+function set_toolchain() {
+    # Add toolchain folders to PATH and request path override (PO environment variable)
+    case "$(id -un)@$(uname -n)" in
+        nathan@archlinux-* | nathan@debian-* | nathan@MSI | nathan@Ryzen-9-3900X | nathan@ubuntu-*) [[ -d ${CBL_LLVM_BNTL:?} ]] && TC_PATH=${CBL_LLVM_BNTL} ;;
+    esac
+    export PATH="${PO:+${PO}:}${TC_PATH:+${TC_PATH}:}${PATH}"
+
+    # Use ccache if it exists
+    CCACHE=$(command -v ccache)
+
+    # Set default values if user did not supply them above
+    true \
+        "${AR:=llvm-ar}" \
+        "${CC:=${CCACHE:+ccache }clang}" \
+        "${HOSTAR:=llvm-ar}" \
+        "${HOSTCC:=${CCACHE:+ccache }clang}" \
+        "${HOSTCXX:=${CCACHE:+ccache }clang++}" \
+        "${HOSTLD:=ld.lld}" \
+        "${HOSTLDFLAGS:=-fuse-ld=lld}" \
+        "${JOBS:="$(nproc)"}" \
+        "${LD:=ld.lld}" \
+        "${LLVM:=1}" \
+        "${LLVM_IAS:=0}" \
+        "${NM:=llvm-nm}" \
+        "${O:=${BASE}/build/${ARCH}}" \
+        "${OBJCOPY:=llvm-objcopy}" \
+        "${OBJDUMP:=llvm-objdump}" \
+        "${OBJSIZE:=llvm-size}" \
+        "${READELF:=llvm-readelf}" \
+        "${STRIP:=llvm-strip}"
+
+    # Resolve O=
+    O=$(readlink -f -m "${O}")
+
+    printf '\n\e[01;32mToolchain location:\e[0m %s\n\n' "$(dirname "$(command -v "${CC##* }")")"
+    printf '\e[01;32mToolchain version:\e[0m %s \n\n' "$("${CC##* }" --version | head -n1)"
+}
+
+function kmake() {
+    set -x
+    time make \
+        -C "${BASE}" \
+        -"${SILENT_MAKE_FLAG:-}"kj"${JOBS}" \
+        AR="${AR}" \
+        ARCH="${ARCH}" \
+        CC="${CC}" \
+        HOSTAR="${AR}" \
+        HOSTCC="${HOSTCC}" \
+        HOSTCXX="${HOSTCXX}" \
+        HOSTLD="${HOSTLD}" \
+        HOSTLDFLAGS="${HOSTLDFLAGS}" \
+        LD="${LD}" \
+        LLVM="${LLVM}" \
+        LLVM_IAS="${LLVM_IAS}" \
+        NM="${NM}" \
+        O="$(realpath -m --relative-to="${BASE}" "${O}")" \
+        OBJCOPY="${OBJCOPY}" \
+        OBJDUMP="${OBJDUMP}" \
+        OBJSIZE="${OBJSIZE}" \
+        READELF="${READELF}" \
+        STRIP="${STRIP}" \
+        ${V:+V=${V}} \
+        "${@}"
+    set +x
+}
+
+function build_kernel() {
+    # Build silently by default
+    ${VERBOSE:=false} || SILENT_MAKE_FLAG=s
+
+    # Configure the kernel
+    CONFIG_MAKE_TARGETS=("${CONFIG##*/}")
+    ${INCREMENTAL:=false} || CONFIG_MAKE_TARGETS=(distclean "${CONFIG_MAKE_TARGETS[@]}")
+    kmake "${CONFIG_MAKE_TARGETS[@]}"
+
+    ${UPDATE_CONFIG_ONLY:=false} && FINAL_TARGET=savedefconfig
+    FINAL_MAKE_TARGETS=(olddefconfig "${FINAL_TARGET:=all}")
+    kmake "${FINAL_MAKE_TARGETS[@]}"
+
+    if ${UPDATE_CONFIG_ONLY}; then
+        cp -v "${O}"/defconfig "${BASE}"/${CONFIG}
+        exit 0
+    fi
+
+    # Let the user know where the kernel will be (if we built one)
+    KERNEL=${O}/arch/${ARCH}/boot/${KERNEL_IMAGE}
+    [[ -f ${KERNEL} ]] && printf '\n\e[01;32mKernel is now available at:\e[0m %s\n' "${KERNEL}"
+}
+
+parse_parameters "${@}"
+set_toolchain
+build_kernel
